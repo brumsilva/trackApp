589c124122518758371af63d760f8ea68fdec3c3
brunosilva <brnsadv@gmail.com>
2023-01-17 19:27:08 -0300

    estrutura e arquitetura do projeto.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+import fastify from 'fastify';
+import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client';
+
+const app = fastify();
+const prisma = new PrismaClient();
+
+app.register(cors);
+
+app.get('/', async (request, reply) => {
+  const habit = await prisma.habit.findMany();
+
+  return habit
+});
+
+
+app.listen({port: 3333}).then(() => {console.log('listening on port 3333')});
